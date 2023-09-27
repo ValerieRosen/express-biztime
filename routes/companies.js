dcf6d03d675d43f2b7d15eae86f39697662b472e
Valerie R <vrosenhauer@gmail.com>
2023-09-27 17:48:58 -0400

    added company code route

--- routes/companies.js
+++ routes/companies.js
@@ -20,3 +20,27 @@ router.get("/", async function (req, res, next) {
     return next(err);
   }
 });
+
+//GET {company: {code, name, description}} add 404
+
+router.get("/:code", async function (req, res, next) {
+  try {
+    let code = req.params.code;
+
+    const compResult = await db.query(
+      `SELECT code, name, description
+            FROM companies
+            WHERE code = $1`,
+      [code]
+    );
+    if (compResult.rows.length === 0) {
+      throw new ExpressError(`No such company: ${code}`, 404);
+    }
+
+    const company = compResult.rows[0];
+
+    return res.json({ company: company });
+  } catch (err) {
+    return next(err);
+  }
+});
