bf84caff663d759ef2aff8b6699acf60e887c109
Valerie R <vrosenhauer@gmail.com>
2023-09-27 20:51:38 -0400

    added put route to edit company

--- routes/companies.js
+++ routes/companies.js
@@ -64,3 +64,28 @@ router.post("/", async function (req, res, next) {
     return next(err);
   }
 });
+
+//PUT {name, description} update object {company: {code, name, description}}
+
+router.put("/:code", async function (req, res, next) {
+  try {
+    let { name, description } = req.body;
+    let code = req.params.code;
+
+    const result = await db.query(
+      `UPDATE companies
+        SET name= $1, description=$2
+        WHERE code = $3
+        RETURNING code, name, description`,
+      [name, description, code]
+    );
+
+    if (result.rows.length === 0) {
+      throw new ExpressError(`No such company: ${code}`, 404);
+    } else {
+      return res.json({ company: result.rows[0] });
+    }
+  } catch (err) {
+    return next(err);
+  }
+});
